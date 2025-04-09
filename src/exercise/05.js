16ea5634579fc18e3055a7c5b16045f38ffa4e11
unbent <daniel.polan@hotmail.com>
2025-04-09 11:05:32 -0400

    Exercise 05 - Extra Credit 02

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -6,12 +6,17 @@ import {Switch} from '../switch'
 
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
+const actionTypes = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
+
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.reset: {
       return initialState
     }
     default: {
@@ -29,8 +34,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actionTypes.toggle})
+  const reset = () => dispatch({type: actionTypes.reset, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -55,15 +60,20 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
     getResetterProps,
   }
 }
+// export {useToggle, toggleReducer, actionTypes}
 
-// Would need to export toggleReducer and import it into the app
+
+// Would need to export toggleReducer and actionTypes and import it into the app
+
+
+// import {useToggle, toggleReducer, actionTypes} from './use-toggle'
 
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && clickedTooMuch) {
+    if (action.type === actionTypes.toggle && clickedTooMuch) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
